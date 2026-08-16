512efc8677243df139064cde60399f088a81e466
Duyn <leduyndp@gmail.com>
2026-08-16 11:42:49 +0700

    Remove server/dist build output files

--- server/dist/routes/executionRoutes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const executionController_1 = require("../controllers/executionController");
-const auth_1 = require("../middleware/auth");
-const router = (0, express_1.Router)();
-router.post('/:testCaseId/execute', auth_1.authenticate, executionController_1.ExecutionController.executeTestCase);
-router.get('/:testCaseId/history', auth_1.authenticate, executionController_1.ExecutionController.getHistory);
-exports.default = router;
--- server/dist/routes/settingRoutes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const settingController_1 = require("../controllers/settingController");
-const auth_1 = require("../middleware/auth");
-const router = (0, express_1.Router)();
-router.get('/environments', settingController_1.SettingController.getEnvironments);
-router.post('/environments', auth_1.optionalAuthenticate, settingController_1.SettingController.saveEnvironments);
-exports.default = router;
